f4a09f808ca6149c7d3080347ab28ed8410ee769
wangxiaoqing <xiaoqingwang@126.com>
2015-08-31 18:05:40 +0800

    加上动画切换效果

--- jyjp/WebBaseViewController.swift
+++ jyjp/WebBaseViewController.swift
@@ -14,8 +14,12 @@ class WebBaseViewController: UIViewController,UIWebViewDelegate {
     var webjs: WebJsHelper?
     var url = ""
     var myWebView:UIWebView?
-    var preloadWebView:UIWebView!;
    
+    var timer: NSTimer?
+    var img1: UIImageView?
+    var img2: UIImageView?
+    var imgindex = true;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         webjs = WebJsHelper();
@@ -41,50 +45,41 @@ class WebBaseViewController: UIViewController,UIWebViewDelegate {
     // 开始加载的时候执行该方法
     func webViewDidStartLoad(webView: UIWebView){
         println("webViewDidStartLoad");
-        var filePath = NSBundle.mainBundle().pathForResource("loading1", ofType: "gif");
-        let preloadGif = NSData(contentsOfFile: filePath!);
-      //  var viewframe = CGRect(origin: CGPoint(x:self.view.frame.width/2-20,y:self.view.frame.height/2-20),size: CGSize(width: 20, height: 20));
-        var viewframe = self.view.frame;
-        self.preloadWebView = UIWebView(frame: viewframe);
-        
-        if let path = NSBundle.mainBundle().pathForResource("index", ofType: "html",inDirectory:"www") {
-            if let htmlData = NSData(contentsOfFile: path){
-                var htmlString = NSString(data:htmlData,encoding:NSUTF8StringEncoding)!
-                let nshtmdata = htmlString.dataUsingEncoding(NSUTF8StringEncoding)
-
-                let baseURL = NSURL.fileURLWithPath(path.stringByDeletingLastPathComponent,isDirectory:true)
-                self.preloadWebView .loadData(nshtmdata, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
-            }
-        }
-       // self.preloadWebView.alpha = 50;
-        self.preloadWebView.opaque = false
-        self.preloadWebView.backgroundColor = UIColor.clearColor()
-        self.myWebView?.addSubview(preloadWebView);
-
+        self.img1 = UIImageView(frame: CGRect(x: 0, y: 0, width: 63, height: 63))
+        self.img1?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        self.img1?.image = UIImage(named: "wb_loading_frame1")
+        self.img2 = UIImageView(frame: CGRect(x: 0, y: 0, width: 63, height: 63))
+        self.img2?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+        self.img2?.image = UIImage(named: "wb_loading_frame2")
+        self.img1?.backgroundColor = UIColor.clearColor()
+        self.img2?.backgroundColor = UIColor.clearColor()
+        self.myWebView?.addSubview(self.img1!)
+        self.myWebView?.addSubview(self.img2!)
+        self.img1?.hidden = true
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "timingFunction:", userInfo: nil, repeats: true)
+        self.timer?.fire()
     }
     
 
     
     func dismissPreloader(){
-            
-            UIView.animateWithDuration(0.8, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+        self.timer?.invalidate()
+        self.img1?.removeFromSuperview()
+        self.img2?.removeFromSuperview()
         
-        self.preloadWebView.alpha = 0;
-        
-        }, completion: nil);
-            
-            UIView.animateWithDuration(0.1, delay: 0.8, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-            
-            self.preloadWebView.removeFromSuperview();
-            
-            }, completion: nil);
-            
+    }
+
+
+    func timingFunction(timer: NSTimer) {
+        self.imgindex = !self.imgindex
+        self.img1?.hidden = self.imgindex
+        self.img2?.hidden = !self.imgindex
     }
     
     // 加载完成的时候执行该方法
     func webViewDidFinishLoad(webView: UIWebView){
         println("webViewDidFinishLoad")
-                self.dismissPreloader();
+        self.dismissPreloader();
     }
        
     func refreshPage(){
