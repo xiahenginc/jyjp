b9f732f64e73f3eb84eb74b4568c251f55672156
wangxiaoqing <xiaoqingwang@126.com>
2015-09-01 15:29:42 +0800

    ww

--- jyjp/WebBaseViewController.swift
+++ jyjp/WebBaseViewController.swift
@@ -15,10 +15,10 @@ class WebBaseViewController: UIViewController,UIWebViewDelegate {
     var url = ""
     var myWebView:UIWebView?
    
-    var timer: NSTimer?
-    var img1: UIImageView?
-    var img2: UIImageView?
-    var imgindex = true;
+//    var timer: NSTimer?
+//    var img1: UIImageView?
+//    var img2: UIImageView?
+//    var imgindex = true;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,41 +45,45 @@ class WebBaseViewController: UIViewController,UIWebViewDelegate {
     // 开始加载的时候执行该方法
     func webViewDidStartLoad(webView: UIWebView){
         println("webViewDidStartLoad");
-        self.img1 = UIImageView(frame: CGRect(x: 0, y: 0, width: 63, height: 63))
-        self.img1?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
-        self.img1?.image = UIImage(named: "wb_loading_frame1")
-        self.img2 = UIImageView(frame: CGRect(x: 0, y: 0, width: 63, height: 63))
-        self.img2?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
-        self.img2?.image = UIImage(named: "wb_loading_frame2")
-        self.img1?.backgroundColor = UIColor.clearColor()
-        self.img2?.backgroundColor = UIColor.clearColor()
-        self.myWebView?.addSubview(self.img1!)
-        self.myWebView?.addSubview(self.img2!)
-        self.img1?.hidden = true
-        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "timingFunction:", userInfo: nil, repeats: true)
-        self.timer?.fire()
+//        var widthimg = 140;
+//        self.img1 = UIImageView(frame: CGRect(x: 0, y: 0, width: widthimg, height: widthimg))
+//       // self.img1 = UIImageView()
+//        self.img1?.image = UIImage(named: "wb_loading_frame1")
+//        self.img1?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+//      
+//        self.img2 = UIImageView(frame: CGRect(x: 0, y: 0, width: widthimg, height: widthimg))
+//      //  self.img2 = UIImageView()
+//        self.img2?.image = UIImage(named: "wb_loading_frame2")
+//        self.img2?.center = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+//        self.img1?.backgroundColor = UIColor.clearColor()
+//        self.img2?.backgroundColor = UIColor.clearColor()
+//        self.myWebView?.addSubview(self.img1!)
+//        self.myWebView?.addSubview(self.img2!)
+//        self.img1?.hidden = true
+//        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "timingFunction:", userInfo: nil, repeats: true)
+//        self.timer?.fire()
     }
     
 
     
-    func dismissPreloader(){
-        self.timer?.invalidate()
-        self.img1?.removeFromSuperview()
-        self.img2?.removeFromSuperview()
-        
-    }
-
-
-    func timingFunction(timer: NSTimer) {
-        self.imgindex = !self.imgindex
-        self.img1?.hidden = self.imgindex
-        self.img2?.hidden = !self.imgindex
-    }
+//    func dismissPreloader(){
+//        self.timer?.invalidate()
+//        self.img1?.removeFromSuperview()
+//        self.img2?.removeFromSuperview()
+//        
+//    }
+//
+//
+//    func timingFunction(timer: NSTimer) {
+//        self.imgindex = !self.imgindex
+//        self.img1?.hidden = self.imgindex
+//        self.img2?.hidden = !self.imgindex
+//    }
     
     // 加载完成的时候执行该方法
     func webViewDidFinishLoad(webView: UIWebView){
         println("webViewDidFinishLoad")
-        self.dismissPreloader();
+     //   self.dismissPreloader();
     }
        
     func refreshPage(){
